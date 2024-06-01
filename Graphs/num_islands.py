1f4081616ff37e847cb74fbe7dc82cfa557b92ee
ayub055 <ayyoobmohd@iisc.ac.in>
2024-06-01 16:36:26 +0530

    max Area of island

--- Graphs/num_islands.py
+++ Graphs/num_islands.py
@@ -1,21 +1,26 @@
 class Solution:
-    def bfs(self, grid, visited, r, c):
-        rows, cols = len(grid), len(grid[0])
+    def bfs(self, grid, visited, r, c, area):
+        
+        rows = len(grid)
+        cols = len(grid[0])
         queue = [(r, c)]
         visited.add((r, c))
         
         while queue:
             row, col = queue.pop(0)
+            area += 1
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            
             for dr, dc in directions:
                 r = row + dr
                 c = col + dc
                 
-                if r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r,c)  not in visited:
+                if r in range(rows) and c in range(cols) and (r, c) not in visited and grid[r][c] != 0:
                     queue.append((r, c))
-                    visited.add((r, c))         
-    
+                    visited.add((r, c))
+                    
+        return area
+                    
+                       
     def numIslands(self, grid):
         if grid is None:
             return 0
@@ -34,7 +39,26 @@ class Solution:
         return num_islands
     
     def maxAreaOfIsland(self, grid):
-        pass
+        if grid is None:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        maxArea = 0
+        visited = set()
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    area = 0
+                    area = self.bfs(grid, visited, r, c, area)
+                    maxArea = max(area, maxArea)
+                    count += 1
+                    
+        return maxArea, count
+                    
+    
+   
                         
     
 grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
@@ -49,5 +73,5 @@ grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
 # q = [(1, 2), (1, 4)]
 # print(q.pop())
 s = Solution()
-print(s.numIslands(grid))
+print(s.maxAreaOfIsland(grid))
 
